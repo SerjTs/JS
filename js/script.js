089bcefd645571f1149db71850fcda8034b3b5b5
Serj <postwork@mail.ru>
2018-11-08 16:18:26 +0400

    Решение задач ко 2 уроку.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,63 @@
-let tempCels = Number(prompt("Введите температуру по Цельсию"));
-let tempFareng = 9/5*tempCels+32;
-alert(tempCels + " градусов по Цельсию - это " + tempFareng + " градусов по Фаренгейту");
+let a = 0, b = 2;
+if (a>=0 && b>=0) {
+    alert (a-b);
+}else if (a<0 && b<0) {
+    alert (a*b);
+}else if (a<0 && b>=0 || a>=0 && b<0) {
+    alert(a+b);
+}
 
-let admin;
-let name;
-name="Василий";
-admin = name;
-alert(admin);
 
-alert(1000+"108");
+a=Number(prompt("Введите число в диапазоне от 0 до 15: "));
+switch(a){
+        case 0:
+            alert("a="+a);
+            a=++a;
+        case 1:
+            alert("a="+a);
+            a=++a;
+        case 2:
+            alert("a="+a);
+            a=++a;
+        case 3:
+            alert("a="+a);
+            a=++a;
+        case 4:
+            alert("a="+a);
+            a=++a;
+        case 5:
+            alert("a="+a);
+            a=++a;
+        case 6:
+            alert("a="+a);
+            a=++a;
+        case 7:
+            alert("a="+a);
+            a=++a;
+        case 8:
+            alert("a="+a);
+            a=++a;
+        case 9:
+            alert("a="+a);
+            a=++a;
+        case 10:
+            alert("a="+a);
+            a=++a;
+        case 11:
+            alert("a="+a);
+            a=++a;
+        case 12:
+            alert("a="+a);
+            a=++a;
+        case 13:
+            alert("a="+a);
+            a=++a;
+        case 14:
+            alert("a="+a);
+            a=++a;
+        case 15:
+            alert("15!");
+            break;
+        default:
+            alert("Неверное зачение a");
+}
